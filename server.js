bfe1ad784b4837b5b52d4308973f0393e6968bb5
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-02-05 14:52:14 +0800

    test

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const io = require('socket.io')(server, {
 
 const PORT = process.env.PORT || 9999;
 
-app.use(express.static(path.join(__dirname, '../client/public')));
+app.use(express.static(path.join(__dirname, 'client/public')));
 
 // указываем, что приложение может получать json данные (для req в add.post)
 app.use(express.json());
